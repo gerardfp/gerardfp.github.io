87b6610739d8127a4783ada1ec91e61694f6d840
gerard <gerardfp@gmail.com>
2020-01-20 11:08:33 +0100

    all

--- mp08.bat
+++ mp08.bat
@@ -10,4 +10,5 @@ claat-windows-amd64.exe export 1n3eFtZhFuDGozMf7EOF0wcmrfwwpaFY-q4N-YUQX-oU
 claat-windows-amd64.exe export 1-_w3B-bTSqsFRZGeTTp9rKjiW5n_rhcev9xU8e1TgaU
 
 claat-windows-amd64.exe export 1ANcSRYRGkcPcYtTy5H3n2oEOzRLU-qT4QLWyyGjhWCY
+claat-windows-amd64.exe export 1UqgATdSEAgeUMBjKTWkblU7tweHQzBjRHuHg_os9zDQ
 
