8342214f47e3ead423fb078b63d1f35719b3df0e
Gerard <gerardfp@gmail.com>
2020-06-15 18:17:41 +0200

    ...

---
+++ build.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const glob = require('glob');
+const swig = require('swig-templates');
+
+const codelabFiles = glob.sync('*/codelab.json');
+let codelabs = {};
+for (let i = 0; i < codelabFiles.length; i++) {
+    var codelab = JSON.parse(fs.readFileSync(codelabFiles[i]));
+    console.log(codelab);
+    for(const cat of codelab.category){
+        let [categoryUrl, categoryName] = cat.split(":");
+        console.log(categoryUrl +"  :  " + categoryName);
+        codelabs[cat] = codelabs[cat] || {categoryName: categoryName, categoryUrl: categoryUrl, codelabs: []};
+        codelab.mainTag = codelab.tags[0];
+        codelabs[cat].codelabs.push(codelab);
+    }
+}
+
+console.log(codelabs);
+
+for(const cat in codelabs){
+    const template = fs.readFileSync('template.html');
+    const html = swig.render(template.toString(), { locals: codelabs[cat] });
+    fs.writeFileSync(`${codelabs[cat].categoryUrl}.html`, new Buffer.from(html));    
+}
