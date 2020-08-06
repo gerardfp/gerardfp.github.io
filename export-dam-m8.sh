cff7119dc23f79d94ed2adea7e087a69051e8b0a
gerardfp <gerardfp@gmail.com>
2020-08-06 10:53:25 +0200

    ...

--- export-dam-m8.sh
+++ export-dam-m8.sh
@@ -6,10 +6,10 @@
 ./claat-linux-amd64 export 1aDAy8i2NhoATJ6RSFyeNIkMYzxd8QAsJ6Op4bEmXlnM
 ./claat-linux-amd64 export 1fiuK5vjtPHJa1x4-fDxxndi-dQyGrLZFQPUEhaw7eB4
 ./claat-linux-amd64 export 1PGkr8ixee-pEHIknPeWPWFwGwos8xieJ44-ueyzFJfk
-#./claat-linux-amd64 export 1ChiXfBuX0SNubITlzr_jIMuX22p9XT1SoV9SCOFrMwk
-#./claat-linux-amd64 export 1n3eFtZhFuDGozMf7EOF0wcmrfwwpaFY-q4N-YUQX-oU
-#./claat-linux-amd64 export 1ANcSRYRGkcPcYtTy5H3n2oEOzRLU-qT4QLWyyGjhWCY
+./claat-linux-amd64 export 1ANcSRYRGkcPcYtTy5H3n2oEOzRLU-qT4QLWyyGjhWCY
+./claat-linux-amd64 export 1UqgATdSEAgeUMBjKTWkblU7tweHQzBjRHuHg_os9zDQ
+./claat-linux-amd64 export 1gGz-klrda5N2_dLZXWozqpAp-bfOKe3x2K4OyL_8b9k
+./claat-linux-amd64 export 1pxgZBL5ykh86nK0jy2zc__SkWTlbuvPOcGmcsYK9vns
 #./claat-linux-amd64 export 1UqgATdSEAgeUMBjKTWkblU7tweHQzBjRHuHg_os9zDQ
-#./claat-linux-amd64 export 1gGz-klrda5N2_dLZXWozqpAp-bfOKe3x2K4OyL_8b9k
 #./claat-linux-amd64 export 1ILVFkUxWUsEMyILpwWFt2bEEq9Vg1k7JzCzAcmQ7LkI
 #./claat-linux-amd64 export 1sfnzdCt3P9T0D04y3ZL3pGXeL98fNHu0j7Yrkzmdf6s
