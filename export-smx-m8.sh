91f16120bf8a4f5abbb6eb1dbf6375d5de5e3bde
gerardfp <gerardfp@gmail.com>
2020-08-13 16:46:00 +0200

    smx webtasks

--- export-smx-m8.sh
+++ export-smx-m8.sh
@@ -1,3 +1,4 @@
-./claat-linux-amd64 export 1EFX2rhBlAaYcNRdP1vPCmbuG9-2QWvEeRA2FGMetjDk
+./claat-linux-amd64 export 1i2e9WK1GrdwDg7her1KKQDbCdEEx-qlq6RPuDn6Mm5k
 ./claat-linux-amd64 export 1yQU2LgoAP8bwXuC1Y6hVWWzHICnpSa4HUV9QtVKZcho
+./claat-linux-amd64 export 1EFX2rhBlAaYcNRdP1vPCmbuG9-2QWvEeRA2FGMetjDk
 ./claat-linux-amd64 export 1hDVF8EinVJgQQ8MFpXN_9EL-VaYHSyjvPQ0XreJywhw
