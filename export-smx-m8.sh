735a7ebbf3f7d93e785ee1638a614eb6ad6b835a
gerardfp <gerardfp@gmail.com>
2020-08-13 12:15:12 +0200

    webfilemanager, webcms, weblms

--- export-smx-m8.sh
+++ export-smx-m8.sh
@@ -1,2 +1,3 @@
 ./claat-linux-amd64 export 1EFX2rhBlAaYcNRdP1vPCmbuG9-2QWvEeRA2FGMetjDk
 ./claat-linux-amd64 export 1yQU2LgoAP8bwXuC1Y6hVWWzHICnpSa4HUV9QtVKZcho
+./claat-linux-amd64 export 1hDVF8EinVJgQQ8MFpXN_9EL-VaYHSyjvPQ0XreJywhw
