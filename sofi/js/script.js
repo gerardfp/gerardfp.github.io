e66dfe85d693e1a3973cf3d939f2bc5f9917f625
Gerard Falco <gerardfp@users.noreply.github.com>
2020-09-05 00:46:17 +0000

    sofi alpha

--- sofi/js/script.js
+++ sofi/js/script.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('code,shell').forEach((block) => {
-      block.textContent = block.textContent.replace(/^\n/,'');
-      block.textContent = block.textContent.replace(/\s*$/,'');
-    });
+  document.querySelectorAll('code,incode,shell').forEach((block) => {
+    var i = 0;
+    if(block.childNodes[i] != null && block.childNodes[i].nodeType == Node.TEXT_NODE){
+      block.childNodes[i].textContent = block.childNodes[i].textContent.replace(/^\n/,'');
+    }
+    i = block.childNodes.length-1;
+    if(block.childNodes[i] != null && block.childNodes[i].nodeType == Node.TEXT_NODE){
+      block.childNodes[i].textContent = block.childNodes[i].textContent.replace(/\s*$/,'');
+    }
   });
+
+  document.querySelectorAll('code').forEach((block) => {
+    var attrs = block.getAttributeNames();
+    if(attrs[0] != null){
+      console.log(attrs[0]);
+      block.classList.add(attrs[0]);
+    }
+    hljs.highlightBlock(block);
+  });
+  
+  document.querySelectorAll('incode').forEach((block) => {
+    var attrs = block.getAttributeNames();
+    if(attrs[0] != null){
+      console.log(attrs[0]);
+      block.classList.add(attrs[0]);
+    }
+    hljs.highlightBlock(block);
+  });
+
+});
